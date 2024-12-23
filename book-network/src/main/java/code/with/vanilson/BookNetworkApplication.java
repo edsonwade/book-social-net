fcd97f788104cbf56c1d7adf03e4c031353ed700
vanilson muhongo <vanilsonmuhongo@gmail.com>
2024-12-23 22:39:32 +0000

    ### [2024-12-23] - Complete security setup with JWT authentication
    - Implemented JWT-based authentication for API endpoints.
    - Configured Spring Security to support JWT authentication and authorization.
    - Added JWT filters and security configurations to handle token parsing and validation.
    - Secured user access with JWT tokens for improved security.

--- book-network/src/main/java/code/with/vanilson/BookNetworkApplication.java
+++ book-network/src/main/java/code/with/vanilson/BookNetworkApplication.java
@@ -2,8 +2,10 @@ package code.with.vanilson;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class BookNetworkApplication {
 
     public static void main(String[] args) {
---
+++ book-network/src/main/java/code/with/vanilson/role/RoleRepository.java
@@ -0,0 +1,9 @@
+package code.with.vanilson.role;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RoleRepository extends JpaRepository<Role, Integer> {
+    Optional<Role> findRoleByName(String roleName);
+}
